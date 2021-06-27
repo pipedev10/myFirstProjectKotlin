7b88dc108a2c000cd7997cc420bf33ad80b4c2b6
Felipe Carrasco <felipe@bakers.dev>
2021-06-26 21:02:48 -0400

    learn different type of data

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,6 @@
 // CONSTANTE
-
 const val PI = 3.1416
+
 fun main(args: Array<String>) {
     println("Hello World!")
     var dinero = 10
@@ -14,4 +14,34 @@ fun main(args: Array<String>) {
     //nombre = "Luis"
     println("nombre $nombre")
     println("CONSTANTE $PI")
+
+    // Type of data
+    // Boolean
+    val boolean : Boolean = true
+    //LONG -> a lot of number of add in the last L example: 9L
+    val numberLong: Long = 3L
+    // DOUBLE
+    val double: Double = 2.7182
+    // FLOAT
+    val float: Float = 1.1f
+
+    // when you assign value to variable you can use different functionalities
+    val firstValue = 20
+    //firstValue.and()
+    //firstValue.compareTo()
+    val secondValue = 10
+    var thirdValue = firstValue.minus(secondValue)
+    println("thirdValue 1 : $thirdValue")
+    thirdValue = firstValue - secondValue
+    println("thirdValue 2 :  $thirdValue")
+
+    // characters
+    val apellido = "Carrasco"
+    val name  = "Felipe"
+    // concatenación una manera
+    var fullName = name + apellido
+    println("fullname 1: $fullName")
+    // concatenación segunda manera
+    fullName = "my name is $name and my lastname $apellido"
+    println("fullname 2: $fullName")
 }
