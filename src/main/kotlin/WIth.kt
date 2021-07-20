834c0a9ac898fe892115b8d16cc2024e3500d45c
ext_lfcarrascog <luis.carrasco@tsoftlatam.com>
2021-07-20 14:49:17 -0400

    19 feature: learn about scope function with

---
+++ src/main/kotlin/WIth.kt
@@ -0,0 +1,18 @@
+// Learn With
+fun main(args: Array<String>) {
+    // scope functions
+    // es otro tipo de scope function, nos permite acceder directamente a las propiedades de una varible.
+    // que le pasamos como parametro, de esta forma evitamos repetir llamar a la variables.
+    // usando this o it
+
+    // example
+
+    val colores = listOf("azul", "amarillo", "rojo")
+
+    with(colores) {
+        println("nuestros colores son $this")
+        println("esta lista tiene una cantidad de colores $size")
+    }
+}
+
+
